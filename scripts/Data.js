66f5b9f8d2bf031254205a17628e953c985677fd
Nate Z <ntzeitlin@gmail.com>
2024-12-17 11:27:24 -0600

    created getData(endpoint) function to facilitate data transfer from api to modules

---
+++ scripts/Data.js
@@ -0,0 +1,9 @@
+// returns data from database.json based
+
+export const getData = async (endpoint) => {
+    const url = `http://localhost:8088/${endpoint}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+}
