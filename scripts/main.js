d00bf1aaac97ebfdf253b7d9cbc067ef585ed675
Nate Z <ntzeitlin@gmail.com>
2024-12-17 14:13:51 -0600

    fully fleshed out index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,45 @@
-const mainContainerElement = document.getElementById("container")
 
-const render = async () => {
+const mainContainerElement = document.querySelector("#container")
+
+const governorListHTML = ""
+const colonyMineralsHTML = ""
+const facilitiesListHTML = ""
+const facilityMineralsHTML = ""
+const shoppingCartHTML = ""
+const purchaseButton = ""
+
+const render = () => {
     const compositeHTML =
-        `<h1>TESTING HEADER</h1>`
+        `<h1 id="title">Solar System Mining Marketplace</h1>
+      <section class="choices">
+        <article class="choices_gov options">
+            <h5>Choose a governor:</h5>
+            ${governorListHTML}
+        </article>
+        <article class="colonies_list options">
+            <h2>Colony Minerals</h2>
+            ${colonyMineralsHTML}
+        </article>
+        <article class="choices_facility options">
+            <h5>Choose a facility:</h5>
+            ${facilitiesListHTML}
+        </article>
+      </section>
 
-    mainContainerElement.innerHTMl = compositeHTML
+      <section class="display">
+        <article class="choices_mineral options">
+            <h3>Facility Minerals</h3>
+            ${facilityMineralsHTML}
+        </article>
+        <article class="cart">
+            <h3>Space Cart</h3>
+            ${shoppingCartHTML}
+            ${purchaseButton}
+        </article>
+      </section>
+    `
+    mainContainerElement.innerHTML = compositeHTML
+
 }
 
 render()
