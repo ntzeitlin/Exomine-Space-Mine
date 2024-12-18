16e7624ea796a538148f8ce7f77a98b92748f42b
Sabra Kozlina <sjkozlina@gmail.com>
2024-12-18 10:54:12 -0700

    trying to fix bugs

--- scripts/generateGovernorList.js
+++ scripts/generateGovernorList.js
@@ -2,10 +2,9 @@ import { getData } from "./Data.js"
 import { transientState, setTransientState } from "./TransientState.js";
 
 const handleGovernorChange = (governorSelectedChangeEvent) => {
-    if (governorSelectedChangeEvent.target.id === "governor" && governorSelectedChangeEvent.target.value != 0) {
+    if (governorSelectedChangeEvent.target.id === "governor") {
         const convertedToInteger = parseInt(governorSelectedChangeEvent.target.value)
         setTransientState("governorId", convertedToInteger)
-        document.getElementById('facility').removeAttribute("disabled")
         }
     }
 
@@ -14,26 +13,26 @@ const handleGovernorChange = (governorSelectedChangeEvent) => {
 export const generateGovernorList = async () => {
     const govData = await getData("governors");
 
-    document.addEventListener("change", handleGovernorChange)
-
+    
     let govListHTML = ""
-
+    
     govListHTML += '<select id="governor">'
     govListHTML += '<option value="0">Choose a governor</option>'
-
+    
     const governorArray = govData
-        .filter((governor) => governor.activestatus === true)
-        .map((governor) => {
-            if (governor.id === transientState.get("governorId")) {
-                setTransientState("colonyId", governor.colonyId)
-                return `<option value="${governor.id}" selected>${governor.name}</option>`
-            }
-            return `<option value="${governor.id}">${governor.name}</option>`
+    .filter((governor) => governor.activestatus === true)
+    .map((governor) => {
+        if (governor.id === transientState.get("governorId")) {
+            setTransientState("colonyId", governor.colonyId)
+            return `<option value="${governor.id}" selected>${governor.name}</option>`
         }
-        )
+        return `<option value="${governor.id}">${governor.name}</option>`
+    }
+)
 
     govListHTML += governorArray.join("")
     govListHTML += "</select>"
 
+    document.addEventListener("change", handleGovernorChange)
     return govListHTML
 }
